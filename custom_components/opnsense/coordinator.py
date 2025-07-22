2fb846121012fc7c9a7215248828cb00f1ca22d3
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-07-22 18:11:00 -0400

    Add async_setup to coordinator

--- custom_components/opnsense/coordinator.py
+++ custom_components/opnsense/coordinator.py
@@ -70,6 +70,16 @@ class OPNsenseDataUpdateCoordinator(DataUpdateCoordinator):
         )
         self._categories = self._build_categories()
 
+    async def _async_setup(self) -> None:
+        """Set up the coordinator."""
+        _LOGGER.debug(
+            "Setting up %sCoordinator",
+            "DT " if self._device_tracker_coordinator else "",
+        )
+        # await self._client.get_host_firmware_version() # Already triggered in init
+        await self._client.set_use_snake_case()
+        # TODO: Get lease types to use
+
     async def _get_states(self, categories: list) -> MutableMapping[str, Any]:
         state: MutableMapping[str, Any] = {}
         total_time: float = 0
